a928bc23c80fbe8161423112ff12315f80484833
Poonga <bpoongs1982@gmail.com>
2023-01-21 12:41:50 -0500

    Initial Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,6 +15,7 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
+    //let ac: AppComponent = new AppComponent();
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
